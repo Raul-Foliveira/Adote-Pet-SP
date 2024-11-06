01386d5eb80464580ae21c58726b1256e57085e1
Raul Florentino <raul.foliveira12@gmail.com>
2024-11-06 17:31:03 -0300

    Incluindo uma API para informações das raças de cahorros

--- Adote-SP/components/banner.js
+++ Adote-SP/components/banner.js
@@ -1,15 +1,20 @@
+import { useRouter } from 'next/router';
 import styles from '../styles/banner.module.css';
 
-
-
 const Banner = () => {
-  return (
-    <section className={styles.banner}>
-      <h1>Encontre seu novo melhor amigo</h1>
-      <p>Adote cães e gatos em São Paulo e dê um lar cheio de amor.</p>
-      <button>Ver Animais Disponíveis</button>
-    </section>
-  );
+    const router = useRouter();
+
+    const handleViewDogs = () => {
+        router.push('/dogs');  // Redireciona para /dogs
+    };
+
+    return (
+        <section className={styles.banner}>
+            <h1>Encontre seu novo melhor amigo</h1>
+            <p>Adote cães e gatos em São Paulo e dê um lar cheio de amor.</p>
+            <button onClick={handleViewDogs}>Ver Animais Disponíveis</button>
+        </section>
+    );
 };
 
 export default Banner;
