d7f22676aba212c983c8215a9956ca178ff48d6a
Raul Florentino <raul.foliveira12@gmail.com>
2024-11-11 16:30:01 -0300

    Alterando o banner na home

--- Adote-SP/components/banner.js
+++ Adote-SP/components/banner.js
@@ -4,9 +4,16 @@ import styles from '../styles/banner.module.css';
 const Banner = () => {
 
     return (
-        <section className={styles.banner}>
-            <h1>Encontre seu novo melhor amigo</h1>
-            <p>Adote cães e gatos em São Paulo e dê um lar cheio de amor.</p>
+        <section className={styles.banner}>  
+            <div className={styles.bannerText}>
+                <h1>Encontre seu novo <strong>melhor amigo</strong></h1>
+                <h2>Adote cães e gatos em São Paulo e dê um lar cheio de amor.</h2>
+                <p>Faça a diferença Adotar é transformar vidas — tanto a sua quanto a deles!</p>
+                <p >Quando você adota, não está apenas salvando um animal,</p> 
+                <p>mas está enriquecendo sua vida com lealdade e amor incondicional.</p>
+                <p >Eles são companheiros, guardiões e trazem alegria para o seu lar.</p> 
+                <p>Ao adotar, você dá ao seu novo amigo a oportunidade de ter um lar cheio de carinho e cuidado.</p>
+            </div>
             <button>Ver Animais Disponíveis</button>
         </section>
     );
